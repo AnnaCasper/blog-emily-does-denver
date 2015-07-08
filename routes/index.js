13643e648a2881b6446b0f4d01f1ff7baf0ce56f
Anna Casper <anna.casper3@gmail.com>
2015-07-08 11:46:02 -0600

    added admin index page

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+//GET about page
 router.get('/about', function(req, res, next){
   res.render('about');
 });
 
+//GET admin login page
 router.get('/admin/login', function(req, res, next){
   res.render('admin/login');
+});
+
+//POST admin login
+router.post('/admin/login', function(req, res, next){
+  res.redirect('/admin');
 })
 
 module.exports = router;
