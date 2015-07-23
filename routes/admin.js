72be72cafbcfc7414aecb207530f5751423454f1
Anna Casper <anna.casper3@gmail.com>
2015-07-22 18:02:25 -0600

    admin can delete posts

--- routes/admin.js
+++ routes/admin.js
@@ -28,18 +28,39 @@ router.post('/new', function(req, res, next){
   blogCollection.insert({title: req.body.title,
     date: req.body.date,
     content: req.body.content}, function(err, data){
-      res.redirect('/admin');
+      res.redirect('/admin/show');
     });
 });
 
 //GET show page
 router.get('/show', function(req, res, next){
-  res.render('admin/show');
+  blogCollection.find({}, function(err, blogs){
+    res.render('admin/show', {blogs: blogs});
+  })
 });
 
 //GET edit page
-router.get('/edit', function(req, res, next){
-  res.render('admin/show');
+router.get('/:id/edit', function(req, res, next){
+  blogCollection.findOne({_id: req.params.id}, function(err, blog){
+    res.render('admin/edit', {blog: blog});
+  })
+});
+
+//POST edits
+router.post('/:id/edit', function(req, res, next){
+  blogCollection.update({_id: req.params.id}, { $set: {
+    title: req.body.title,
+    date: req.body.date,
+    content: req.body.content
+  }}, function(err, data){
+    res.redirect('/admin/show');
+  })
+})
+
+//POST delete
+router.post('/:id/delete', function(req, res, next){
+  blogCollection.remove({_id: req.params.id});
+  res.redirect('/admin/show')
 });
 
 //GET logout
