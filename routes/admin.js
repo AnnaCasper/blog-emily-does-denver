931c623e965002c957ea74857dbbdcebcc232d31
Anna Casper <anna.casper3@gmail.com>
2015-08-08 12:36:58 -0600

    added login and cookies

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var db = require('monk')(process.env.MONGOLAB_URI);
 var blogCollection = db.get('blogs');
 var userCollection = db.get('users')
+var authorization = require('express-authorization');
+var bcrypt = require('bcryptjs');
 
 var validations = require("../lib/javascripts/validations");
 
@@ -11,9 +13,30 @@ router.get('/login', function(req, res, next){
   res.render('admin/login');
 });
 
+//POST login
+router.post('/login', function(req, res, next){
+  userCollection.findOne({ email: req.body.email.toUpperCase()}, function(err, data){
+    if (data == null){
+      res.render('admin/login', { errors: "There is no account associated with this email. Please create a new account."});
+    } else {
+      var compare = bcrypt.compareSync(req.body.password, data.password);
+      if (compare === true){
+        res.cookie('currentUser', data._id);
+        res.redirect('/admin');
+      } else {
+        res.render('admin/login', {errors: "Email and password do not match."})
+      };
+    };
+  });
+});
+
 //GET admin index page
 router.get('/', function(req, res, next){
-  res.render('admin');
+  if(req.cookies.currentUser){
+    res.render('admin');
+  } else {
+    res.redirect('/admin/login')
+  }
 });
 
 //POST admin login
@@ -41,18 +64,15 @@ router.post('/signup', function(req, res, next){
       var hash = bcrypt.hashSync(req.body.password, 8);
       userCollection.insert({
         user_name: req.body.user_name,
-        profile_pic: "",
         email: email,
         password: hash,
-        challenge_ids: [],
-        scores: []
         });
       userCollection.findOne({email: email}, function(err, data){
         res.cookie('currentUser', data._id);
-        res.redirect('/challenges');
+        res.redirect('/admin');
       });
     } else {
-        res.render('users/new', {
+        res.render('admin/signup', {
           errors: errors,
           user_name: req.body.user_name,
           email: email
@@ -61,9 +81,19 @@ router.post('/signup', function(req, res, next){
   })
 })
 
+//GET logout
+router.get('/logout', function(req, res, next){
+  res.clearCookie('currentUser');
+  res.render('admin/login');
+});
+
 //GET new post page
 router.get('/new', function(req, res, next){
-  res.render('admin/new');
+  if(req.cookies.currentUser){
+    res.render('admin/new');
+  } else {
+    res.redirect('/admin/login')
+  }
 });
 
 //POST new blog
@@ -77,16 +107,25 @@ router.post('/new', function(req, res, next){
 
 //GET show page
 router.get('/show', function(req, res, next){
-  blogCollection.find({}, function(err, blogs){
-    res.render('admin/show', {blogs: blogs});
-  })
+  if(req.cookies.currentUser){
+    blogCollection.find({}, function(err, blogs){
+      res.render('admin/show', {blogs: blogs});
+    })
+  } else {
+    res.redirect('/admin/login')
+  }
+
 });
 
 //GET edit page
 router.get('/:id/edit', function(req, res, next){
-  blogCollection.findOne({_id: req.params.id}, function(err, blog){
-    res.render('admin/edit', {blog: blog});
-  })
+  if(req.cookies.currentUser){
+    blogCollection.findOne({_id: req.params.id}, function(err, blog){
+      res.render('admin/edit', {blog: blog});
+    })
+  } else {
+    res.redirect('/admin/login')
+  }
 });
 
 //POST edits
@@ -101,14 +140,16 @@ router.post('/:id/edit', function(req, res, next){
 })
 
 //POST delete
-router.post('/:id/delete', function(req, res, next){
-  blogCollection.remove({_id: req.params.id});
-  res.redirect('/admin/show')
+router.post('/:id/delete', function(req, res, next){  
+  if(req.cookies.currentUser){
+    blogCollection.findOne({_id: req.params.id}, function(err, blog){
+      res.render('admin/edit', {blog: blog});
+    })
+  } else {
+    res.redirect('/admin/login')
+  }
 });
 
-//GET logout
-router.get('/logout', function(req, res, next){
-  res.render('admin/login');
-})
+
 
 module.exports = router;
